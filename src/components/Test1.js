605502cd4f8f56402ec6ef617d0adfc7c689c358
모하영 <mohayoung80@gmail.com>
2026-03-18 08:42:04 +0900

    test hi

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Test1 = () => {
     return (
         <div>
-            <button>오늘의 버튼입니다</button>
+            <button>오늘의 버튼입니다. 예니 안녕</button>
         </div>
     );
 };
