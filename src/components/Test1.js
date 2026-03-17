b9d1c7a9a59f6a72c431e1a4e2c0113c7c76368d
project1 <yen1k1m124@naver.com>
2026-03-18 08:41:14 +0900

    살려주세요

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Test1 = () => {
     return (
         <div>
-            <button>오늘의 버튼입니다</button>
+            안녕하세요 조장님 처음 만난순간부터 이 말을 하고 싶었어 이 학원에서 선배님을 만난순간부터
         </div>
     );
 };
