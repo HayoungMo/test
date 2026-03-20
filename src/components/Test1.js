3097ad4b62bc299be3e06ea348a272dda26a0ff8
dlsghsla4623-cloud <dlsghsla4623@gmail.com>
2026-03-20 17:32:27 +0900

    이름수정

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Test1 = () => {
     return (
         <div>
-            안녕하세요 조장님 처음 만난순간부터 이 말을 하고 싶었어 이 학원에서 선배님을 만난순간부터
-        </div>
+           김인호 
+                </div>
     );
 };
 
