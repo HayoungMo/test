fc5bfc5f970d4e50d8a3467c5a2d07fb5755ed8e
yen1 <yen1k1m124@naver.com>
2026-03-23 17:26:08 +0900

    gkgk
    sork QKffkTwy>

--- src/components/Test2.js
+++ src/components/Test2.js
@@ -4,7 +4,7 @@ const Test2 = () => {
     return (
         <div>
             <button>누르면 학교가 폭발하는 버튼</button>
-            <li>학교는 폭팔시키는 곳이 아닙니다</li>
+            <button>행운의 편지 발사</button>
             하이하이
             확인
         </div>
